e9d9c5eda0ccac078df02e72672394a198e58aac
MC LOC <mcloc@localhost.localdomain>
2019-02-17 19:17:43 -0300

    gambiarra de newLine pra concatenar 2 leituras de uma string devido a problemas de comunicacao serial

--- src/br/com/sintechs/stufaSerialRead/Main.java
+++ src/br/com/sintechs/stufaSerialRead/Main.java
@@ -1,7 +1,11 @@
 package br.com.sintechs.stufaSerialRead;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.Logger;
 
 import com.fazecast.jSerialComm.SerialPort;
@@ -17,13 +21,13 @@ public class Main {
 	static public void main(String[] args) throws Exception {
 		SerialPort comPort = SerialPort.getCommPorts()[0];
 		comPort.openPort();
-		comPort.setBaudRate(19200);
+		comPort.setBaudRate(57600);
 
 		while (comPort.bytesAvailable() == -1) {
 			if (!comPort.isOpen()) {
 				comPort.closePort();
 				comPort.openPort();
-				comPort.setBaudRate(19200);
+				comPort.setBaudRate(57600);
 			} else {
 				throw new Exception("port is closed, check TIMEOUT");
 			}
@@ -31,29 +35,52 @@ public class Main {
 		}
 
 		try {
+			StringBuilder data = new StringBuilder();
 			while (true) {
-				if (comPort.bytesAvailable() == 0)
-					continue;
-//				System.out.println("avaliable: " + comPort.bytesAvailable());
-				byte[] readBuffer = new byte[comPort.bytesAvailable()];
-				int totalReaded = comPort.readBytes(readBuffer, comPort.bytesAvailable());
-
-				if (totalReaded != readBuffer.length)
-					throw new Exception("total lido diferente do disponivel");
-//				for (int i = 0; i < readBuffer.length; i++) {
+				
+				while (comPort.bytesAvailable() > 0) {
+					byte[] readBuffer = new byte[comPort.bytesAvailable()];
+					int totalReaded = comPort.readBytes(readBuffer, comPort.bytesAvailable());
+					 System.out.println("total buffer: " + totalReaded);
+					if (totalReaded != readBuffer.length)
+						throw new Exception("total lido diferente do disponivel");
 					String s = new String(readBuffer, "UTF-8");
-					System.out.println(s );
-//				}
-				// if(!s.contains("\n"))
-				// continue;
-				// if(s.length() > 0)
-				// System.out.println("ZERO");
-				// System.out.println("string: " + s);
+					
+					if(s.contains("\n"))  {
+						data.append(s); 
+						System.out.println("string: " + data);
+						data = new StringBuilder();
+						break;
+						//throw new Exception("tem \\n ");
+					}
+//					s.replace("\n", "");
+//					s.replace("\r", "");
+//					s.replace("\t", "");
+					data.append(s); 
+					
+				}
+				if(data.length() == 0)
+					data = new StringBuilder();
+				else {
+//					System.out.println(data);
+//					writeInSHMFile(data.toString());
+				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		comPort.closePort();
+	}
+
+	private static void writeInSHMFile(String s) {
+		 try {
+			 try (FileChannel outChan = new FileOutputStream(SHM_ADDRESS_READ, true).getChannel()) {
+			      outChan.truncate(0);
+			    }
+	            Files.write(Paths.get(SHM_ADDRESS_READ), s.getBytes());
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
 	}
 
 	private static void checkSHMFiles() throws Exception {
