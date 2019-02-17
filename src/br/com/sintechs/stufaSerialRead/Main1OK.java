b4c775d74e4f94e5bd7639c631a1b7d4ee29b2a6
MC LOC <mcloc@localhost.localdomain>
2019-02-17 17:39:57 -0300

    falta remover \n de dentro do Json

--- src/br/com/sintechs/stufaSerialRead/Main1OK.java
+++ src/br/com/sintechs/stufaSerialRead/Main1OK.java
@@ -2,58 +2,38 @@ package br.com.sintechs.stufaSerialRead;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.Logger;
 
 import com.fazecast.jSerialComm.SerialPort;
 
-public class Main {
-
+public class Main1OK {
+	
 	static Logger _log = Logger.getGlobal();
 	static final String SHM_ADDRESS_READ = "/dev/shm/serial2arduinoRead";
 	static final String SHM_ADDRESS_WRITE = "/dev/shm/serial2arduinoWrite";
 	static final String SHM_ADDRESS_READ_LOCK = "/dev/shm/serial2arduinoReadLock";
 	static final String SHM_ADDRESS_WRITE_LOCK = "/dev/shm/serial2arduinoWriteLock";
-
-	static public void main(String[] args) throws Exception {
+	
+	public static void main(String args[]) throws Exception {
+		_log.info("Starting messageReader...");
+		checkSHMFiles();
+		
 		SerialPort comPort = SerialPort.getCommPorts()[0];
+		comPort.setBaudRate(500000);
 		comPort.openPort();
-		comPort.setBaudRate(19200);
-
-		while (comPort.bytesAvailable() == -1) {
-			if (!comPort.isOpen()) {
-				comPort.closePort();
-				comPort.openPort();
-				comPort.setBaudRate(19200);
-			} else {
-				throw new Exception("port is closed, check TIMEOUT");
-			}
-			break;
-		}
-
-		try {
-			while (true) {
-				if (comPort.bytesAvailable() == 0)
-					continue;
-//				System.out.println("avaliable: " + comPort.bytesAvailable());
-				byte[] readBuffer = new byte[comPort.bytesAvailable()];
-				int totalReaded = comPort.readBytes(readBuffer, comPort.bytesAvailable());
-
-				if (totalReaded != readBuffer.length)
-					throw new Exception("total lido diferente do disponivel");
-//				for (int i = 0; i < readBuffer.length; i++) {
-					String s = new String(readBuffer, "UTF-8");
-					System.out.println(s );
-//				}
-				// if(!s.contains("\n"))
-				// continue;
-				// if(s.length() > 0)
-				// System.out.println("ZERO");
-				// System.out.println("string: " + s);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+		InputStream in = comPort.getInputStream();
+		try
+		{
+		   for (int j = 0; j < 56; ++j)
+		      System.out.print((char)in.read());
+		   in.close();
+		} catch (Exception e) { e.printStackTrace(); }
+		
 		comPort.closePort();
+		
+		
 	}
 
 	private static void checkSHMFiles() throws Exception {
@@ -96,7 +76,7 @@ public class Main {
 				e.printStackTrace();
 				throw new Exception(e);
 			}
-
+		
 	}
 
 }
