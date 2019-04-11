a5367d8a605747b1e79762e5d5299ee21b7686ea
Mario Caseiro <mcl.caseiro@gmail.com>
2019-04-11 12:10:18 -0300

    crash recovery done

--- src/br/com/sintechs/stufaSerialRead/Main.java
+++ src/br/com/sintechs/stufaSerialRead/Main.java
@@ -19,10 +19,66 @@ public class Main {
 	static public void main(String[] args) throws Exception {
 		_log.log(Level.INFO, "Starting Arduino Serial Reader...");
 		
+		StringBuilder data = new StringBuilder();
+		SerialPort comPort;
+		comPort = waitForPortConnection();
+		comPort.openPort();
+		comPort.setBaudRate(115200);
+		_log.log(Level.INFO, "Communications started...");
+		while(true) {
+			try {
+				readDataFromPort(comPort, data);
+			} catch (ArrayIndexOutOfBoundsException e) {
+				_log.log(Level.SEVERE, e.getMessage());
+				comPort.closePort();
+			}
+			while (comPort.bytesAvailable() == -1) {
+				if (!comPort.isOpen()) {
+					comPort.closePort();
+					comPort.openPort();
+					comPort.setBaudRate(115200);
+					_log.log(Level.INFO, "Communications restarted...");
+				} else {
+					comPort.closePort();
+					_log.log(Level.SEVERE, "Communication lost, maybe device was disconected, waiting for port ttyUSB0 show up again...");
+					comPort = waitForPortConnection();
+//					throw new Exception("port is closed, check TIMEOUT, ha, there is no Timeout on this lib");
+				}
+			}
+			Thread.sleep(20);
+		} // END While(TRUE)
+	}
+
+	private static void readDataFromPort(SerialPort comPort, StringBuilder data) throws Exception {
+			while (comPort.bytesAvailable() > 0) {
+				byte[] readBuffer = new byte[comPort.bytesAvailable()];
+				int totalReaded = comPort.readBytes(readBuffer, comPort.bytesAvailable());
+				if (totalReaded != readBuffer.length)
+					throw new Exception("index out of bound on readBytes (-1) returned, it's a comm problem");
+				
+				String s = new String(readBuffer, "UTF-8");
+				if(s.contains("\n") || s.contains("\r") || s.contains("\t"))  {
+					s.replace("\n", "");
+					s.replace("\r", "");
+					s.replace("\t", "");
+					s.replaceAll("\\r$", "");
+					data.append(s); 
+					writeInSHMFile(data.toString());
+					data.setLength(0);
+					data = new StringBuilder();
+					Thread.sleep(20);
+					return;
+				}
+				data.append(s); 
+			} // END of while(communication in)
+			if(data.length() == 0)
+				data = new StringBuilder();
+	}
+
+	private static SerialPort waitForPortConnection() throws InterruptedException {
 		SerialPort[] conn = null;
 		SerialPort comPort = null;
 		boolean searchingPorts = true;
-		
 		
 		while(searchingPorts) {
 			
@@ -60,83 +116,9 @@ public class Main {
 			Thread.sleep(2000);
 		}
 		
-		comPort.openPort();
-		comPort.setBaudRate(115200);
-
-		while (comPort.bytesAvailable() == -1) {
-			if (!comPort.isOpen()) {
-				comPort.closePort();
-				comPort.openPort();
-				comPort.setBaudRate(115200);
-			} else {
-				throw new Exception("port is closed, check TIMEOUT, ha, there is no Timeout on this lib");
-			}
-			break;
-		}
-		_log.log(Level.INFO, "Communications started...");
-		try {
-			StringBuilder data = new StringBuilder();
-			while (true) {
-				while (comPort.bytesAvailable() > 0) {
-					byte[] readBuffer = new byte[comPort.bytesAvailable()];
-					try {
-						int totalReaded = comPort.readBytes(readBuffer, comPort.bytesAvailable());
-						if (totalReaded != readBuffer.length)
-							throw new Exception("total readed diferen of avaliable");
-					} catch (ArrayIndexOutOfBoundsException e) {
-						_log.warning("index out of bound on readBytes (-1) returned, it's a comm problem");
-						break;
-					}
-//					 System.out.println("total buffer: " + totalReaded);
-					
-					String s = new String(readBuffer, "UTF-8");
-					
-					if(s.contains("\n") || s.contains("\r") || s.contains("\t"))  {
-						s.replace("\n", "");
-						s.replace("\r", "");
-						s.replace("\t", "");
-						s.replaceAll("\\r$", "");
-						data.append(s); 
-//						System.out.println("string: " + data);
-						writeInSHMFile(data.toString());
-						data.setLength(0);
-						data = new StringBuilder();
-						break;
-						//throw new Exception("tem \\n ");
-					}
-
-					data.append(s); 
-					
-				} // END of while(communication in)
-				if(data.length() == 0)
-					data = new StringBuilder();
-				
-				Thread.sleep(20);
-			} // END of while(true)
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		comPort.closePort();
+		return comPort;
 	}
 
-	private static boolean openPorts(SerialPort[] conn) throws InterruptedException {
-		conn = SerialPort.getCommPorts();
-		if(conn == null || conn.length <= 0 ) {
-			if(firstRun) {
-				_log.log(Level.WARNING, "No ports were found. Check if the device is connected...");
-				firstRun = false;
-			}
-			else
-				_log.log(Level.FINER, "No ports were found. Check if the device is connected...");
-			
-			Thread.sleep(2000);
-			return false;
-		}
-		
-		return true;
-		
-	}
 
 	private static void writeInSHMFile(String s) {
 		try (FileOutputStream file = new FileOutputStream(SHM_ADDRESS_READ, false)) {
